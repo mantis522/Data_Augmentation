9541c0c5997339e71b9b3f7f1edba40c6c8f7141
mantis522 <augustnorase@gmail.com>
2021-09-24 02:12:53 +0900

    tiny

--- model_implementation/GRU_test.py
+++ model_implementation/GRU_test.py
@@ -118,38 +118,38 @@ base_data = base_data.drop(['Unnamed: 0'], axis=1)
 
 text_encoding = base_data['original_text']
 
-# original_text_df = base_data['original_text']
-# original_label_df = base_data['original_label']
-# summarized_text_df = base_data['summarized_text']
-# huggingface_sentiment_df = base_data['huggingface_sentiment']
-#
-# num_list = []
-# non_num_list = []
-#
-# for i in range(len(original_text_df)):
-#     if original_label_df[i] == huggingface_sentiment_df[i]:
-#         num_list.append(i)
-#     else:
-#         non_num_list.append(i)
-#
-# train_df = base_data.loc[num_list]
-# test_df = base_data.loc[non_num_list]
-#
-# # 훈련 데이터는 4만개, 테스트 데이터는 1만개.
-# #
-#
-# cal_per = len(train_df)
-# cal_per = int(cal_per)
+original_text_df = base_data['original_text']
+original_label_df = base_data['original_label']
+summarized_text_df = base_data['summarized_text']
+huggingface_sentiment_df = base_data['huggingface_sentiment']
 
-# train_df = train_df[:cal_per]
-# train_origin_df = train_df.drop(['summarized_text', 'huggingface_sentiment'], axis=1)
-# train_aug_df = train_df.drop(['original_text', 'original_label'], axis=1)
-#
-# train_aug_df.columns = ['original_text', 'original_label']
-#
-# train_df = pd.concat([train_origin_df, train_aug_df])
+num_list = []
+non_num_list = []
 
-# text_encoding = train_df['original_text']
+for i in range(len(original_text_df)):
+    if original_label_df[i] == huggingface_sentiment_df[i]:
+        num_list.append(i)
+    else:
+        non_num_list.append(i)
+
+train_df = base_data.loc[num_list]
+test_df = base_data.loc[non_num_list]
+
+# 훈련 데이터는 4만개, 테스트 데이터는 1만개.
+#
+
+cal_per = len(train_df)
+cal_per = int(cal_per)
+
+train_df = train_df[:cal_per]
+train_origin_df = train_df.drop(['summarized_text', 'huggingface_sentiment'], axis=1)
+train_aug_df = train_df.drop(['original_text', 'original_label'], axis=1)
+
+train_aug_df.columns = ['original_text', 'original_label']
+
+train_df = pd.concat([train_origin_df, train_aug_df])
+
+text_encoding = train_df['original_text']
 
 
 t = Tokenizer()
--- model_implementation/test.py
+++ model_implementation/test.py
@@ -5,24 +5,106 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, GRU, Embedding
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
 
-vocab_size = 10000
-(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words = vocab_size)
 
-max_len = 500
-X_train = pad_sequences(X_train, maxlen=max_len)
-X_test = pad_sequences(X_test, maxlen=max_len)
+file_path = r"D:\ruin\data\imdb_summarization\t5_base_with_huggingface_sentiment.csv"
+glove_path = r"D:\ruin\data\glove.6B\glove.6B.100d.txt"
+
+imdb_csv = file_path
+base_data = pd.read_csv(imdb_csv)
+base_data = base_data.drop(['Unnamed: 0'], axis=1)
+
+text_encoding = base_data['original_text']
+
+original_text_df = base_data['original_text']
+original_label_df = base_data['original_label']
+summarized_text_df = base_data['summarized_text']
+huggingface_sentiment_df = base_data['huggingface_sentiment']
+
+num_list = []
+non_num_list = []
+
+for i in range(len(original_text_df)):
+    if original_label_df[i] == huggingface_sentiment_df[i]:
+        num_list.append(i)
+    else:
+        non_num_list.append(i)
+
+train_df = base_data.loc[num_list]
+test_df = base_data.loc[non_num_list]
+
+cal_per = len(train_df)
+cal_per = int(cal_per)
+
+train_df = train_df[:cal_per]
+# train_df = train_df.drop(['summarized_text', 'huggingface_sentiment'], axis=1)
+train_origin_df = train_df.drop(['summarized_text', 'huggingface_sentiment'], axis=1)
+train_aug_df = train_df.drop(['original_text', 'original_label'], axis=1)
+
+train_aug_df.columns = ['original_text', 'original_label']
+
+train_df = pd.concat([train_origin_df, train_aug_df])
+
+t = Tokenizer()
+t.fit_on_texts(text_encoding)
+
+vocab_size = len(t.word_index) + 1
+sequences = t.texts_to_sequences(text_encoding)
+
+def max_text():
+    for i in range(1, len(sequences)):
+        max_length = len(sequences[0])
+        if len(sequences[i]) > max_length:
+            max_length = len(sequences[i])
+    return max_length
+
+text_num = max_text()
+
+
+maxlen = text_num
+batch_size = 128
+epochs = 15
+
+test_df, val_df = train_test_split(test_df, test_size=0.5, random_state=0)
+
+
+def making_dataset(data_df):
+    x_train = data_df['original_text'].values
+    x_train = t.texts_to_sequences(x_train)
+    x_train = sequence.pad_sequences(x_train, maxlen=maxlen, padding='post')
+    y_train = data_df['original_label'].values
+    y_train = to_categorical(np.asarray(y_train))
+
+    return x_train, y_train
+
+x_train, y_train = making_dataset(train_df)
+x_test, y_test = making_dataset(test_df)
+x_val, y_val = making_dataset(val_df)
+
+print('X_train size:', x_train.shape)
+print('y_train size:', y_train.shape)
+print('X_test size:', x_test.shape)
+print('y_test size:', y_test.shape)
+print('X_val size: ', x_val.shape)
+print('y_val size: ', y_val.shape)
+
 
 model = Sequential()
 model.add(Embedding(vocab_size, 100))
 model.add(GRU(128))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(2, activation='softmax'))
 
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=2)
 mc = ModelCheckpoint('GRU_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
 
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=60, validation_split=0.2)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+history = model.fit(x_train, y_train, epochs=15, callbacks=[es, mc], batch_size=60, validation_data=(x_val, y_val))
 
 loaded_model = load_model('GRU_model.h5')
-print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
+print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(x_test, y_test)[1]))
