e57bd703b26d6c9fe41e13a476d434d8a04fece0
mantis522 <augustnorase@gmail.com>
2021-12-08 04:17:03 +0900

    practice_start

--- test_code/test.py
+++ test_code/test.py
@@ -1,12 +1,12 @@
-from sys import stdin, stdout
+import tweepy
 
-a = stdin.readline()
-A = set(stdin.readline().split())
-b = stdin.readline()
-B = stdin.readline().split()
+consumer_key = '3h7gTcpHsJqUVEpDfyiaFtk2c'
+consumer_secret = 'uBEcCZbwZecgnTVWEhAMw3EIdRxps8Zx5cHOOM2ldrG8R1txrT'
+access_token = '1059852994731106304-nZPxMcG0TbutEhfIFqeWdv4eVTrvLa'
+access_token_secret = 'Wd3X87eeF65SsetcWSHIEz22JorutzzWt4SyTzEUfsqaQ'
 
-for i in B:
-    if i in A:
-        stdout.write('1\n')
-    else:
-        stdout.write('0\n')
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
