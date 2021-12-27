7edc74f4727fbeea9d637a457cf1ab1c69358b11
mantis522 <augustnorase@gmail.com>
2021-12-27 22:58:42 +0900

    text_dataset_from_directory 따라서 구현해보려고 했는데 포맷을 어떻게 바꾸는지 모르겠음

--- test_code/test2.py
+++ test_code/test2.py
@@ -1,47 +1,67 @@
 import tensorflow as tf
 import pandas as pd
+import numpy as np
+import tensorflow_hub as hub
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-import numpy as np
-from collections import Counter
-import tensorflow_text as text
-import tensorflow_hub as hub
 from official.nlp import optimization
-
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
+import tensorflow_text as text
+import os
 
 file_path = r"D:\ruin\data\IMDB Dataset2.csv"
+
 imdb_csv = file_path
 df_imdb = pd.read_csv(imdb_csv)
 df_imdb = df_imdb.drop(['Unnamed: 0'], axis=1)
 
-y = np.int32(df_imdb.label.astype('category').cat.codes.to_numpy())
-num_classes = np.unique(y).shape[0]
+train_df, test_df = train_test_split(df_imdb, test_size=0.2, random_state=0)
+test_df, val_df = train_test_split(test_df, test_size=0.5, random_state=0)
 
-max_features = 10000
-output_dim = 16
+train_text = train_df['text'].to_numpy()
+tok = Tokenizer(oov_token='<unk>')
+tok.fit_on_texts(train_text)
+tok.word_index['<pad>'] = 0
+tok.index_word[0] = '<pad>'
 
-results = Counter()
-df_imdb['text'].str.split().apply(results.update)
-vocabulary = [key[0] for key in results.most_common(max_features)]
+train_seqs = tok.texts_to_sequences(train_text)
+train_seqs = tf.keras.preprocessing.sequence.pad_sequences(train_seqs, padding='post')
 
-tokenizer = Tokenizer(num_words=max_features)
-tokenizer.fit_on_texts(vocabulary)
+train_labels = train_df['label'].to_numpy().flatten()
 
-X = tokenizer.texts_to_sequences(df_imdb['text'].values)
-X = pad_sequences(X)
+test_text = test_df['text'].to_numpy()
+test_seqs = tok.texts_to_sequences(test_text)
+test_seqs = tf.keras.preprocessing.sequence.pad_sequences(test_seqs, padding='post')
 
-max_input_lenght = X.shape[1]
+test_labels = test_df['label'].to_numpy().flatten()
 
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-x_test, x_val, y_test, y_val = train_test_split(x_test, y_test, test_size=0.2, random_state=0)
+valid_text = val_df['text'].to_numpy()
+valid_seqs = tok.texts_to_sequences(valid_text)
+valid_seqs = tf.keras.preprocessing.sequence.pad_sequences(valid_seqs, padding='post')
 
-print('X_train size:', x_train.shape)
-print('y_train size:', y_train.shape)
-print('X_test size:', x_test.shape)
-print('y_test size:', y_test.shape)
-print('X_val size: ', x_val.shape)
-print('y_val size: ', y_val.shape)
+valid_labels = val_df['label'].to_numpy().flatten()
+
+# CONVERT TO TF DATASETS
+
+train_ds = tf.data.Dataset.from_tensor_slices((train_seqs, train_labels))
+test_ds = tf.data.Dataset.from_tensor_slices((test_seqs, test_labels))
+valid_ds = tf.data.Dataset.from_tensor_slices((valid_seqs, valid_labels))
+
+BUFFER_SIZE = 100
+BATCH_SIZE = 100
+
+train_ds = train_ds.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+valid_ds = valid_ds.batch(BATCH_SIZE)
+
+# PREFETCH
+
+train_ds = train_ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+test_ds = test_ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+valid_ds = valid_ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+print(train_ds)
 
 bert_model_name = 'small_bert/bert_en_uncased_L-4_H-512_A-8'  #@param ["bert_en_uncased_L-12_H-768_A-12", "bert_en_cased_L-12_H-768_A-12", "bert_multi_cased_L-12_H-768_A-12", "small_bert/bert_en_uncased_L-2_H-128_A-2", "small_bert/bert_en_uncased_L-2_H-256_A-4", "small_bert/bert_en_uncased_L-2_H-512_A-8", "small_bert/bert_en_uncased_L-2_H-768_A-12", "small_bert/bert_en_uncased_L-4_H-128_A-2", "small_bert/bert_en_uncased_L-4_H-256_A-4", "small_bert/bert_en_uncased_L-4_H-512_A-8", "small_bert/bert_en_uncased_L-4_H-768_A-12", "small_bert/bert_en_uncased_L-6_H-128_A-2", "small_bert/bert_en_uncased_L-6_H-256_A-4", "small_bert/bert_en_uncased_L-6_H-512_A-8", "small_bert/bert_en_uncased_L-6_H-768_A-12", "small_bert/bert_en_uncased_L-8_H-128_A-2", "small_bert/bert_en_uncased_L-8_H-256_A-4", "small_bert/bert_en_uncased_L-8_H-512_A-8", "small_bert/bert_en_uncased_L-8_H-768_A-12", "small_bert/bert_en_uncased_L-10_H-128_A-2", "small_bert/bert_en_uncased_L-10_H-256_A-4", "small_bert/bert_en_uncased_L-10_H-512_A-8", "small_bert/bert_en_uncased_L-10_H-768_A-12", "small_bert/bert_en_uncased_L-12_H-128_A-2", "small_bert/bert_en_uncased_L-12_H-256_A-4", "small_bert/bert_en_uncased_L-12_H-512_A-8", "small_bert/bert_en_uncased_L-12_H-768_A-12", "albert_en_base", "electra_small", "electra_base", "experts_pubmed", "experts_wiki_books", "talking-heads_base"]
 
@@ -200,23 +220,6 @@ print(f'Word Ids   : {text_preprocessed["input_word_ids"][0, :12]}')
 print(f'Input Mask : {text_preprocessed["input_mask"][0, :12]}')
 print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
 
-tfhub_handle_encoder = map_name_to_handle[bert_model_name]
-tfhub_handle_preprocess = map_model_to_preprocess[bert_model_name]
-
-print(f'BERT model selected           : {tfhub_handle_encoder}')
-print(f'Preprocess model auto-selected: {tfhub_handle_preprocess}')
-
-bert_preprocess_model = hub.KerasLayer(tfhub_handle_preprocess)
-
-text_test = ['this is such an amazing movie!']
-text_preprocessed = bert_preprocess_model(text_test)
-
-print(f'Keys       : {list(text_preprocessed.keys())}')
-print(f'Shape      : {text_preprocessed["input_word_ids"].shape}')
-print(f'Word Ids   : {text_preprocessed["input_word_ids"][0, :12]}')
-print(f'Input Mask : {text_preprocessed["input_mask"][0, :12]}')
-print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
-
 bert_model = hub.KerasLayer(tfhub_handle_encoder)
 
 bert_results = bert_model(text_preprocessed)
@@ -242,10 +245,31 @@ classifier_model = build_classifier_model()
 bert_raw_result = classifier_model(tf.constant(text_test))
 print(tf.sigmoid(bert_raw_result))
 
-loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-metrics = tf.metrics.BinaryAccuracy()
+# tf.keras.utils.plot_model(classifier_model)
+
+
 
 epochs = 5
-steps_per_epoch = tf.data.experimental.cardinality(x_train).numpy()
+steps_per_epoch = tf.data.experimental.cardinality(train_ds).numpy()
 num_train_steps = steps_per_epoch * epochs
 num_warmup_steps = int(0.1*num_train_steps)
+
+init_lr = 3e-5
+optimizer = optimization.create_optimizer(init_lr=init_lr,
+                                          num_train_steps=num_train_steps,
+                                          num_warmup_steps=num_warmup_steps,
+                                          optimizer_type='adamw')
+
+classifier_model.compile(optimizer=optimizer,
+                         loss='categorical_crossentropy',
+                         metrics='acc')
+
+print(f'Training model with {tfhub_handle_encoder}')
+history = classifier_model.fit(train_ds,
+                               validation_data=valid_ds,
+                               epochs=epochs)
+
+loss, accuracy = classifier_model.evaluate(test_ds)
+
+print(f'Loss: {loss}')
+print(f'Accuracy: {accuracy}')
