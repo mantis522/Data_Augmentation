2bf8fc8447846942be5d5c0a9ef3af07b3528078
mantis522 <augustnorase@gmail.com>
2021-12-29 17:50:22 +0900

    이게 그나마 시간 적게 잡아먹는 BERT 모델

--- model_implementation/BERT_IMDB.py
+++ model_implementation/BERT_IMDB.py
@@ -1,200 +1,160 @@
-import os
-import numpy as np
-from tqdm import tqdm
-import tensorflow as tf
-from transformers import BertTokenizer, TFBertModel
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import re
+import os
+from tokenizers import BertWordPieceTokenizer
+from transformers import BertTokenizer
 
-# 코드는 문제가 없는데 문장의 길이가 길면 OOM이 걸림.
-# 그렇다고 배치사이즈를 너무 줄이면 학습하는데 시간도 오래걸리고 성능도 쓰레기됨
-# 배치사이즈 8, 시퀀스길이 308 쯤에는 문제 없이 돌아감. 그런데 시간이 너무 오래걸림
+MAX_SEQ_LEN = 305
 
-
-tf.random.set_seed(1234)
-np.random.seed(1234)
-
-file_path = r"D:\ruin\data\IMDB Dataset2.csv"
-
-imdb_csv = file_path
-df_imdb = pd.read_csv(imdb_csv)
-df_imdb = df_imdb.drop(['Unnamed: 0'], axis=1)
-
-text_encoding = df_imdb['text']
-t = Tokenizer()
-t.fit_on_texts(text_encoding)
-vocab_size = len(t.word_index) + 1
-sequences = t.texts_to_sequences(text_encoding)
-
-def max_text():
-    for i in range(1, len(sequences)):
-        max_length = len(sequences[0])
-        if len(sequences[i]) > max_length:
-            max_length = len(sequences[i])
-    return max_length
-
-text_num = max_text()
-maxlen = text_num
+def get_masks(tokens):
+    """Masks: 1 for real tokens and 0 for paddings"""
+    return [1] * len(tokens) + [0] * (MAX_SEQ_LEN - len(tokens))
 
 
-BATCH_SIZE = 16
-NUM_EPOCHS = 3
-VALID_SPLIT = 0.2
-MAX_LEN = 100 # EDA에서 추출된 Max Length
-DATA_IN_PATH = 'data_in/KOR'
-DATA_OUT_PATH = "../test_code/data_out/KOR"
+def get_segments(tokens):
+    """Segments: 0 for the first sequence, 1 for the second"""
+    segments = []
+    current_segment_id = 0
+    for token in tokens:
+        segments.append(current_segment_id)
+        if token == "[SEP]":
+            current_segment_id = 1
+    return segments + [0] * (MAX_SEQ_LEN - len(tokens))
+
+def get_ids(ids):
+    """Token ids from Tokenizer vocab"""
+    token_ids = ids
+    input_ids = token_ids + [0] * (MAX_SEQ_LEN - len(token_ids))
+    return input_ids
+
+def create_single_input(sentence, tokenizer):
+    """Create an input from a sentence"""
+    encoded = tokenizer.encode(sentence)
+
+    ids = get_ids(encoded.ids)
+    masks = get_masks(encoded.tokens)
+    segments = get_segments(encoded.tokens)
+
+    return ids, masks, segments
+
+def convert_sentences_to_features(sentences, tokenizer):
+    """Convert sentences to features: input_ids, input_masks and input_segments"""
+    input_ids, input_masks, input_segments = [], [], []
+
+    for sentence in tqdm(sentences, position=0, leave=True):
+        ids, masks, segments = create_single_input(sentence, tokenizer)
+        assert len(ids) == MAX_SEQ_LEN
+        assert len(masks) == MAX_SEQ_LEN
+        assert len(segments) == MAX_SEQ_LEN
+        input_ids.append(ids)
+        input_masks.append(masks)
+        input_segments.append(segments)
+
+    return [np.asarray(input_ids, dtype=np.int32), np.asarray(input_masks, dtype=np.int32), np.asarray(input_segments, dtype=np.int32)]
+
+def nlp_model(callable_object):
+    # Load the pre-trained BERT base model
+    bert_layer = hub.KerasLayer(handle=callable_object, trainable=True)
+
+    # BERT layer three inputs: ids, masks and segments
+    input_ids = Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name="input_ids")
+    input_masks = Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name="input_masks")
+    input_segments = Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name="segment_ids")
+
+    inputs = [input_ids, input_masks, input_segments]  # BERT inputs
+    pooled_output, sequence_output = bert_layer(inputs)  # BERT outputs
+
+    # Add a hidden layer
+    x = Dense(units=768, activation="relu")(pooled_output)
+    x = Dropout(0.1)(x)
+
+    # Add output layer
+    outputs = Dense(2, activation="softmax")(x)
+
+    # Construct a new model
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+model = nlp_model("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2")
+model.summary()
 
 
-tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased", cache_dir='../test_code/bert_ckpt', do_lower_case=False)
+if __name__ == '__main__':
+    file_path = r"D:\ruin\data\IMDB Dataset2.csv"
 
-train_df, test_df = train_test_split(df_imdb, test_size=0.2, random_state=0)
-test_df, val_df = train_test_split(test_df, test_size=0.5, random_state=0)
+    imdb_df = pd.read_csv(file_path)
+    df_imdb = imdb_df.drop(['Unnamed: 0'], axis=1)
 
-train_df = train_df[:1000]
+    train_df, test_df = train_test_split(df_imdb, test_size=0.2, random_state=0)
+    test_df, val_df = train_test_split(test_df, test_size=0.5, random_state=0)
 
-def bert_tokenizer(sent, MAX_LEN):
-    encoded_dict = tokenizer.encode_plus(
-        text=sent,
-        add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
-        max_length=MAX_LEN,  # Pad & truncate all sentences.
-        pad_to_max_length=True,
-        return_attention_mask=True  # Construct attn. masks.
+    TAG_RE = re.compile(r"<[^>]+>")
 
-    )
+    def remove_tags(text):
+        return TAG_RE.sub("", text)
 
-    input_id = encoded_dict['input_ids']
-    attention_mask = encoded_dict[
-        'attention_mask']  # And its attention mask (simply differentiates padding from non-padding).
-    token_type_id = encoded_dict['token_type_ids']  # differentiate two sentences
+    def preprocess_text(sen):
+        # Removing html tags
+        sentence = remove_tags(sen)
+        # Remove punctuations and numbers
+        sentence = re.sub("[^a-zA-Z]", " ", sentence)
+        # Single character removal
+        sentence = re.sub(r"\s+[a-zA-Z]\s+", " ", sentence)
+        # Removing multiple spaces
+        sentence = re.sub(r"\s+", " ", sentence)
+        return sentence
 
-    return input_id, attention_mask, token_type_id
+    def preprocessing_data(dataframe):
+        reviews = []
+        sentences = list(dataframe['text'])
+        for sen in sentences:
+            reviews.append(preprocess_text(sen))
 
+        reviews = convert_sentences_to_features(reviews, tokenizer)
 
-input_ids = []
-attention_masks = []
-token_type_ids = []
-train_data_labels = []
+        label = dataframe['label']
+        label = np.array(list(label))
+        label = to_categorical(label)
 
-
-for train_sent, train_label in tqdm(zip(train_df["text"], train_df["label"]), total=len(train_df)):
-    try:
-        input_id, attention_mask, token_type_id = bert_tokenizer(train_sent, MAX_LEN)
-
-        input_ids.append(input_id)
-        attention_masks.append(attention_mask)
-        token_type_ids.append(token_type_id)
-        train_data_labels.append(train_label)
-
-    except Exception as e:
-        print(e)
-        print(train_sent)
-        pass
-
-train_movie_input_ids = np.array(input_ids, dtype=int)
-train_movie_attention_masks = np.array(attention_masks, dtype=int)
-train_movie_type_ids = np.array(token_type_ids, dtype=int)
-train_movie_inputs = (train_movie_input_ids, train_movie_attention_masks, train_movie_type_ids)
-
-train_data_labels = np.asarray(train_data_labels, dtype=np.int32)  # 레이블 토크나이징 리스트
-
-print("# sents: {}, # labels: {}".format(len(train_movie_input_ids), len(train_data_labels)))
-
-input_id = train_movie_input_ids[1]
-attention_mask = train_movie_attention_masks[1]
-token_type_id = train_movie_type_ids[1]
-
-print(input_id)
-print(attention_mask)
-print(token_type_id)
-print(tokenizer.decode(input_id))
+        return reviews, label
 
 
-class TFBertClassifier(tf.keras.Model):
-    def __init__(self, model_name, dir_path, num_class):
-        super(TFBertClassifier, self).__init__()
+    slow_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    save_path = "../test_code/bert_base_uncased/"
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    slow_tokenizer.save_pretrained(save_path)
 
-        self.bert = TFBertModel.from_pretrained(model_name, cache_dir=dir_path)
-        self.dropout = tf.keras.layers.Dropout(self.bert.config.hidden_dropout_prob)
-        self.classifier = tf.keras.layers.Dense(num_class,
-                                                kernel_initializer=tf.keras.initializers.TruncatedNormal(
-                                                    self.bert.config.initializer_range),
-                                                name="classifier")
+    tokenizer = BertWordPieceTokenizer("bert_base_uncased/vocab.txt", lowercase=True)
+    tokenizer.enable_truncation(MAX_SEQ_LEN-2)
 
-    def call(self, inputs, attention_mask=None, token_type_ids=None, training=False):
-        # outputs 값: # sequence_output, pooled_output, (hidden_states), (attentions)
-        outputs = self.bert(inputs, attention_mask=attention_mask, token_type_ids=token_type_ids)
-        pooled_output = outputs[1]
-        pooled_output = self.dropout(pooled_output, training=training)
-        logits = self.classifier(pooled_output)
+    x_train, y_train = preprocessing_data(train_df)
+    x_test, y_test = preprocessing_data(test_df)
+    x_val, y_val = preprocessing_data(val_df)
 
-        return logits
+    BATCH_SIZE = 8
+    EPOCHS = 3
 
-cls_model = TFBertClassifier(model_name='bert-base-multilingual-cased',
-                             dir_path='../test_code/bert_ckpt',
-                             num_class=2)
+    opt = Adam(learning_rate=2e-5)
+    model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
 
-optimizer = tf.keras.optimizers.Adam(3e-5)
-loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
-cls_model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+    history = model.fit(x_train, y_train,
+                        validation_data=(x_val, y_val),
+                        epochs=EPOCHS,
+                        batch_size=BATCH_SIZE,
+                        verbose=1)
 
-model_name = "tf2_bert_imdb"
+    # Save the trained model
+    # model.save('nlp_model.h5')
 
-# overfitting을 막기 위한 ealrystop 추가
-earlystop_callback = EarlyStopping(monitor='val_accuracy', min_delta=0.0001, patience=2)
-# min_delta: the threshold that triggers the termination (acc should at least improve 0.0001)
-# patience: no improvment epochs (patience = 1, 1번 이상 상승이 없으면 종료)\
-
-checkpoint_path = os.path.join(DATA_OUT_PATH, model_name, 'weights.h5')
-checkpoint_dir = os.path.dirname(checkpoint_path)
-
-# Create path if exists
-if os.path.exists(checkpoint_dir):
-    print("{} -- Folder already exists \n".format(checkpoint_dir))
-else:
-    os.makedirs(checkpoint_dir, exist_ok=True)
-    print("{} -- Folder create complete \n".format(checkpoint_dir))
-
-cp_callback = ModelCheckpoint(
-    checkpoint_path, monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=True)
-
-# 학습과 eval 시작
-history = cls_model.fit(train_movie_inputs, train_data_labels, epochs=NUM_EPOCHS, batch_size=BATCH_SIZE,
-                        validation_split=VALID_SPLIT, callbacks=[earlystop_callback, cp_callback])
-
-# steps_for_epoch
-
-print(history.history)
-
-
-input_ids = []
-attention_masks = []
-token_type_ids = []
-test_data_labels = []
-
-for test_sent, test_label in tqdm(zip(test_df["text"], test_df["label"])):
-    try:
-        input_id, attention_mask, token_type_id = bert_tokenizer(test_sent, MAX_LEN)
-
-        input_ids.append(input_id)
-        attention_masks.append(attention_mask)
-        token_type_ids.append(token_type_id)
-        test_data_labels.append(test_label)
-    except Exception as e:
-        print(e)
-        print(test_sent)
-        pass
-
-test_movie_input_ids = np.array(input_ids, dtype=int)
-test_movie_attention_masks = np.array(attention_masks, dtype=int)
-test_movie_type_ids = np.array(token_type_ids, dtype=int)
-test_movie_inputs = (test_movie_input_ids, test_movie_attention_masks, test_movie_type_ids)
-
-test_data_labels = np.asarray(test_data_labels, dtype=np.int32) #레이블 토크나이징 리스트
-
-print("num sents, labels {}, {}".format(len(test_movie_input_ids), len(test_data_labels)))
-
-results = cls_model.evaluate(test_movie_inputs, test_data_labels, batch_size=BATCH_SIZE)
-print("test loss, test acc: ", results)
+    pred_test = np.argmax(model.predict(x_test), axis=1)
+    print(pred_test[:10])
---
+++ test_code/test.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+
+# from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from tqdm import tqdm
+import re
+import os
+from tokenizers import BertWordPieceTokenizer
+from transformers import BertTokenizer
+
+MAX_SEQ_LEN = 305  # max sequence length
+
+
+def get_masks(tokens):
+    """Masks: 1 for real tokens and 0 for paddings"""
+    return [1] * len(tokens) + [0] * (MAX_SEQ_LEN - len(tokens))
+
+
+def get_segments(tokens):
+    """Segments: 0 for the first sequence, 1 for the second"""
+    segments = []
+    current_segment_id = 0
+    for token in tokens:
+        segments.append(current_segment_id)
+        if token == "[SEP]":
+            current_segment_id = 1
+    return segments + [0] * (MAX_SEQ_LEN - len(tokens))
+
+
+def get_ids(ids):
+    """Token ids from Tokenizer vocab"""
+    token_ids = ids
+    input_ids = token_ids + [0] * (MAX_SEQ_LEN - len(token_ids))
+    return input_ids
+
+
+def create_single_input(sentence, tokenizer):
+    """Create an input from a sentence"""
+    encoded = tokenizer.encode(sentence)
+
+    ids = get_ids(encoded.ids)
+    masks = get_masks(encoded.tokens)
+    segments = get_segments(encoded.tokens)
+
+    return ids, masks, segments
+
+
+def convert_sentences_to_features(sentences, tokenizer):
+    """Convert sentences to features: input_ids, input_masks and input_segments"""
+    input_ids, input_masks, input_segments = [], [], []
+
+    for sentence in tqdm(sentences, position=0, leave=True):
+        ids, masks, segments = create_single_input(sentence, tokenizer)
+        assert len(ids) == MAX_SEQ_LEN
+        assert len(masks) == MAX_SEQ_LEN
+        assert len(segments) == MAX_SEQ_LEN
+        input_ids.append(ids)
+        input_masks.append(masks)
+        input_segments.append(segments)
+
+    return [np.asarray(input_ids, dtype=np.int32), np.asarray(input_masks, dtype=np.int32), np.asarray(input_segments, dtype=np.int32)]
+
+
+def nlp_model(callable_object):
+    # Load the pre-trained BERT base model
+    bert_layer = hub.KerasLayer(handle=callable_object, trainable=True)
+
+    # BERT layer three inputs: ids, masks and segments
+    input_ids = Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name="input_ids")
+    input_masks = Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name="input_masks")
+    input_segments = Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name="segment_ids")
+
+    inputs = [input_ids, input_masks, input_segments]  # BERT inputs
+    pooled_output, sequence_output = bert_layer(inputs)  # BERT outputs
+
+    # Add a hidden layer
+    x = Dense(units=768, activation="relu")(pooled_output)
+    x = Dropout(0.1)(x)
+
+    # Add output layer
+    outputs = Dense(2, activation="softmax")(x)
+
+    # Construct a new model
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+model = nlp_model("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2")
+model.summary()
+
+file_path = r"D:\ruin\data\IMDB Dataset2.csv"
+
+movie_reviews = pd.read_csv(file_path)
+
+movie_reviews.head(5)
+movie_reviews = movie_reviews.sample(frac=1)
+movie_reviews = movie_reviews.drop(['Unnamed: 0'], axis=1)
+
+print(movie_reviews.isnull().values.any())
+print(movie_reviews.shape)
+
+def preprocess_text(sen):
+    # Removing html tags
+    sentence = remove_tags(sen)
+
+    # Remove punctuations and numbers
+    sentence = re.sub("[^a-zA-Z]", " ", sentence)
+
+    # Single character removal
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", " ", sentence)
+
+    # Removing multiple spaces
+    sentence = re.sub(r"\s+", " ", sentence)
+
+    return sentence
+
+
+TAG_RE = re.compile(r"<[^>]+>")
+
+
+def remove_tags(text):
+    return TAG_RE.sub("", text)
+
+
+reviews = []
+sentences = list(movie_reviews["text"])
+for sen in sentences:
+    reviews.append(preprocess_text(sen))
+
+
+y = movie_reviews["label"]
+
+y = np.array(list(map(lambda x: 1 if x == 1 else 0, y)))
+
+print(y)
+
+
+
+slow_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+save_path = "bert_base_uncased/"
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+slow_tokenizer.save_pretrained(save_path)
+
+# Load the fast tokenizer from saved file
+tokenizer = BertWordPieceTokenizer("bert_base_uncased/vocab.txt", lowercase=True)
+tokenizer.enable_truncation(MAX_SEQ_LEN - 2)
+
+train_count = 40000  # 40000
+test_count = 2000  #
+
+# X_train = convert_sentences_to_features(reviews[:40000], tokenizer)
+# X_test = convert_sentences_to_features(reviews[40000:], tokenizer)
+
+X_train = convert_sentences_to_features(reviews[:train_count], tokenizer)
+X_test = convert_sentences_to_features(reviews[train_count : train_count + test_count], tokenizer)
+
+one_hot_encoded = to_categorical(y)
+# one_hot_encoded = tf.one_hot(y, 1)
+
+# y_train = one_hot_encoded[:40000]
+# y_test = one_hot_encoded[40000:]
+
+y_train = one_hot_encoded[:train_count]
+y_test = one_hot_encoded[train_count : train_count + test_count]
+
+print(X_train)
+print(y_train)
+
+BATCH_SIZE = 8
+EPOCHS = 3
+
+opt = Adam(learning_rate=2e-5)
+model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
+
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=1)
+
+# Save the trained model
+# model.save('nlp_model.h5')
+
+pred_test = np.argmax(model.predict(X_test), axis=1)
+print(pred_test[:10])
