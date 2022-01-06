ce54310a912cf92033f5c33f311ba2575978a884
mantis522 <augustnorase@gmail.com>
2022-01-06 22:58:25 +0900

    data update

--- for10_imple_codes/for10_aug_attention_biGRU_t5_large.py
+++ for10_imple_codes/for10_aug_attention_biGRU_t5_large.py
@@ -240,8 +240,8 @@ if __name__ == '__main__':
     df_imdb = df_imdb.drop(['Unnamed: 0'], axis=1)
     # df_imdb = df_imdb.sample(frac=1).reset_index(drop=True)
 
-    start = 0
-    end = 2000
+    start = 13000
+    end = 14000
 
     while start < 50000:
         print("present :", start)
@@ -434,7 +434,7 @@ if __name__ == '__main__':
             print("Average accuracy:", average_acc)
 
             now = datetime.datetime.now()
-            csv_filename = r"result\B_Attention\Aug_Attention_biGRU_t5_large2.csv"
+            csv_filename = r"result\B_Attention\Aug_Attention_biGRU_t5_large_Colab.csv"
             result_list = [now, i + 1, len(original_data), len(train_df), start, end, acc, loss,
                            recall, precision, F1_micro, F1_macro, average_acc]
 
@@ -463,6 +463,6 @@ if __name__ == '__main__':
 
             print(i + 1, "번째 학습 끝")
 
-        start = start + 2000
-        end = end + 2000
+        start = start + 1000
+        end = end + 1000
 
