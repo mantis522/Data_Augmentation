ca9a8e38dd259a4a5ca976e6c3aee2af1f9a450a
mantis522 <augustnorase@gmail.com>
2022-01-07 21:32:03 +0900

    data update

--- for10_imple_codes/GRU_Attention/AAB_t5_large.py
+++ for10_imple_codes/GRU_Attention/AAB_t5_large.py
@@ -190,7 +190,7 @@ if __name__ == '__main__':
     df_imdb = df_imdb.drop(['Unnamed: 0'], axis=1)
 
     start = 0
-    end = 1000
+    end = 2000
 
     while start < 50000:
         print("present :", start)
@@ -379,6 +379,6 @@ if __name__ == '__main__':
 
             print(i + 1, "번째 학습 끝")
 
-        start = start + 1000
-        end = end + 1000
+        start = start + 2000
+        end = end + 2000
 
