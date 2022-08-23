48a67b2a9585660f6c7cd0283611c1017f703bd6
mantis522 <augustnorase@gmail.com>
2022-08-24 02:46:22 +0900

    extract_50k

--- preprocess/extract_50000.py
+++ preprocess/extract_50000.py
@@ -1,21 +1,30 @@
-# import pandas as pd
-# from collections import Counter
-#
-# imdb_data = pd.read_csv(r"D:\ruin\data\IMDB Dataset2.csv")
-#
-# text_count = 0
-#
-# for i in range(len(imdb_data)):
-#     text = imdb_data['text'][i]
-#     text_len = len(text)
-#     text_count = text_count + text_len
-#
-# text_avg = text_count / len(imdb_data)
-#
-# print(text_avg)
-
 import pandas as pd
+from tqdm import tqdm
 
-amazon_dataset = pd.read_csv(r"D:\ruin\data\amazon\amazon_review_polarity_csv\train_revise.csv")
+neg_data_name = r"D:\ruin\data\amazon\amazon_review_polarity_csv\amazon_neg_train.csv"
+pos_data_name = r"D:\ruin\data\amazon\amazon_review_polarity_csv\amazon_pos_train.csv"
 
+def making_df(df_name):
+    label_list = []
+    title_list = []
+    text_list = []
+    amazon_data = pd.read_csv(df_name)
 
+    for i in tqdm(range(len(amazon_data))):
+        if len(amazon_data['text'][i]) >= 900:
+            text_list.append(amazon_data['text'][i])
+            label_list.append(amazon_data['sentiment_label'][i])
+            title_list.append(amazon_data['title'][i])
+
+    df = pd.DataFrame((zip(label_list, title_list, text_list)), columns=['sentiment_label', 'title', 'text'])
+
+    return df
+
+amazon_data_neg = making_df(neg_data_name)
+amazon_data_pos = making_df(pos_data_name)
+
+amazon_data_neg = amazon_data_neg[:25000]
+amazon_data_pos = amazon_data_pos[:25000]
+
+amazon_data = pd.concat([amazon_data_neg, amazon_data_pos])
+amazon_data.to_csv(r"D:\ruin\data\amazon\amazon_review_polarity_csv\amazon_50k_data.csv", index=False)
