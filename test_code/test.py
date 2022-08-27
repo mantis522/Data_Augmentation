004c2f4302f91f39ec2213974f119b438fa3500d
mantis522 <augustnorase@gmail.com>
2022-08-27 16:28:24 +0900

    tiny

--- test_code/test.py
+++ test_code/test.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-test_dir = "....csv"
+test_dir = r"D:\ruin\data\amazon\amazon_review_polarity_csv\amazon_t5_base_with_huggingface_sentiment.csv"
 test_data = pd.read_csv(test_dir)
 
 ori_text_list = []
@@ -12,12 +12,11 @@ for i in range(len(test_data)):
     ori_text_list.append(test_data['original_text'][i])
     aug_text_list.append(test_data['summarized_text'][i])
     if test_data['original_label'][i] == 1:
-        # test_data['original_label'][i] = 0
         ori_label_list.append(0)
     elif test_data['original_label'][i] == 2:
-        # test_data['original_label'][i] = 1
         ori_label_list.append(1)
-    elif test_data['huggingface_sentiment'][i] == 1:
+
+    if test_data['huggingface_sentiment'][i] == 1:
         aug_label_list.append(0)
     elif test_data['huggingface_sentiment'][i] == 2:
         aug_label_list.append(1)
