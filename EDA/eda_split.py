b2244831a7ee1d825bdcbf9c677e91db21488683
mantis522 <augustnorase@gmail.com>
2022-11-29 04:14:18 +0900

    eda 입력을 위해 기존 csv 파일에서 500개씩 쪼개서 txt 파일로 저장하는 코드 추가

---
+++ EDA/eda_split.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+csv_file = r"D:\ruin\data\amazon\amazon_review_polarity_csv\amazon_t5_base_with_huggingface_sentiment.csv"
+csv_file = pd.read_csv(csv_file)
+
+csv_file = csv_file.drop(['Unnamed: 0'], axis=1)
+
+start = 0
+end = 500
+
+spot = 500
+
+while start < 50000:
+    original_data = csv_file[start:end]
+
+    origin_text = np.array(original_data['original_text'].tolist())
+    origin_text = list(origin_text)
+    origin_label = np.array(original_data['original_label'].tolist())
+    origin_label = list(origin_label)
+
+    f = open('D:/ruin/data/eda_nlp/' + 'test' + str(end) + '.txt', 'w', encoding='utf-8')
+    for i in range(len(origin_text)):
+        data = str(origin_label[i]) + '\t' + origin_text[i] + '\n'
+        f.write(data)
+    f.close()
+
+    start = start + spot
+    end = end + spot
