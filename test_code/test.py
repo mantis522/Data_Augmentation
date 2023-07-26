362a74dbb7ee9c062d41f7d876f968e4ec258043
mantis522 <augustnorase@gmail.com>
2023-07-26 23:06:46 +0900

    소프트맥스 계산

--- test_code/test.py
+++ test_code/test.py
@@ -1,5 +1,5 @@
 from sklearn.utils.extmath import softmax
 import numpy as np
 
-X = np.array([[12.5, 11.875, 10.625], [8.125, 12.5, 8.125], [12.5, 7.5, 13.75]])
+X = np.array([[50, 60, 66]])
 print(softmax(X.astype(np.double)))
